e2eba03c3d7206c17f61ddc145379e0f1f962310
AlexRikka <rikka.gold@mail.ru>
2024-01-11 00:16:42 +0400

    show description on pokemon page

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,6 +7,7 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True,
                               upload_to='pokemon_images')
+    description = models.TextField(default='')
 
     def __str__(self) -> str:
         return f'{self.title}'
